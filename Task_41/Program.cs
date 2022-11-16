d0fad4851ebec952daf9eb41616dac76dc54a59a
Renat <renatakhmedyanov@gmail.com>
2022-11-16 12:00:27 +0400

    Add Task 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Программа, которая считает количество положительных чисел, введенных пользователем.");
+
+Console.Write("Введите количество чисел: ");
+int M = int.Parse(Console.ReadLine()!);
+
+int CountPositiveNumbers()
+{
+    int count = 0;
+    for (int i = 0; i < M; i++)
+    {
+        int input = int.Parse(Console.ReadLine()!);
+        if (input > 0) count++;
+    }
+    return count;
+}
+int result = CountPositiveNumbers();
+Console.WriteLine("Количество положительных чисел: " + result);
